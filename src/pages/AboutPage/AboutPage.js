a99db8d30f75866eda92c40e121970c0a610a59d
Brendon Kuleck <brendonkuleck@gmail.com>
2018-07-09 13:47:51 -0500

    Better about

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -25,23 +25,26 @@ const AboutPage = (props, context) => {
         <div className="about-wrapper">
           <div className="about-content" style={{ color: textPrimary }}>
             <p>
-              Like many, my humble beginnings lie in the world of NotePad coding (WordPad introduced hidden characters!) and Geocities, Angelfire and Tripod in the mid-90s but have expanded into the modern landscape of robustly-featured internet applications.
+              Like many, my humble beginnings lie in the worlds Geocities, Angelfire and Tripod, coding for fun by hand in NotePad (WordPad introduced hidden characters!) and in the mid-90s but have grown and expanded into the modern landscape of robustly-featured internet applications as the web has developed.
             </p>
             <p>
               My current favorite language is <span className="highlight">JavaScript</span> and I love everything having to do with the open web.
             </p>
             <img src="http://brendonkuleck.com/images/brendon_profile.png" className="profile-img" style={{ display: "inline" }}/>
             <p>
-              HTML5, CSS3, JavaScript (ES6, ECMA2015), JS frameworks (<span className="highlight">React</span>, Redux, Node, Angular, Ember), npm, <span className="highlight">MongoDB</span>, RESTful APIs, babel, jslint, webpack, version control (Git/SVN), AWS are just a few of the tools in my current toolbox.
+              HTML5, CSS3, JavaScript (ES6 // ECMA2015), JS frameworks (<span className="highlight">React</span> (with or without Redux), Node, Angular, Ember), npm, <span className="highlight">MongoDB</span>, RESTful APIs, babel, jslint, webpack, version control (Git/SVN), AWS are just a few of the tools in my current toolbox.
             </p>
             <p>
-              I also enjoy the CLI and digging deeper into its powers. Classically trained in OOP, however I'm getting more and more interested in functional programming as of late and thinking with more of a test-driven development mindset.
+              When it comes to programming paradigms, I'm educated in classical OOP but have been more and more interested in functional programming: first-class functions, higher order functions, functions as arguments/value.
             </p>
             <p>
-              Professionally, I've developed applications such as the <span className="highlight">NiKEiD Creator</span> as a consultant at R/GA, <span className="highlight">Groupon's web and email platforms</span>, the US Dept of Education with Accenture, as well as promotional and interactive websites/apps for Fortune 500 companies like HP, Motorola, Burger King, GE, GM, Walgreen's, Johnnie Walker, Land Rover, United and AARP while at DesignKitchen.
+              Excellent article about thinking in a functional programming way: <a href="https://medium.com/javascript-scene/the-dao-of-immutability-9f91a70c88cd" className="about-link" rel="noreferrer noopener" target="_blank">The Dao of Immutability</a>
             </p>
             <p>
-              In addition this plethroa of overwhelmingly diverse subjects, I also enjoy reading, listening to music, collecting sneakers, fishing, hiking, camping, playing hockey, <span className="highlight">taking pictures of my cat</span> and watching orange and brown ball sports.
+              Professionally, I've developed applications such as the <span className="highlight">NiKEiD Creator</span> as a consultant at R/GA, <span className="highlight">Groupon's web and email platforms as well as internal tools</span>, the US Dept of Education with Accenture and promotional and interactive websites/apps for Fortune 500 companies like HP, Motorola, Burger King, GE, GM, Walgreen's, Johnnie Walker, Land Rover, United and AARP while at DesignKitchen.
+            </p>
+            <p>
+              In addition this plethroa of overwhelmingly diverse subjects, I also enjoy reading, listening to music, collecting sneakers, fishing, hiking, camping, playing hockey, <span className="highlight">taking pictures of my cat</span> and watching orange and brown ball sports. I also love 90s action movies and anything having to do with time travel or personally incomprehensible scientific subjects.
             </p>
             <p>
               Not to mention <a href="https://twitter.com/JalenRose/status/277310949604601856" className="about-link" rel="noreferrer noopener" target="_blank">Jalen Rose once replied to me on Twitter</a>.
