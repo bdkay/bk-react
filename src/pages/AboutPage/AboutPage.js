43c0d33d8ddf3907d570239f1cd9f8db7d1ad2de
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-07 19:59:46 -0500

    On FM

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -25,17 +25,16 @@ const AboutPage = (props, context) => {
         <div className="about-wrapper">
           <div className="about-content" style={{ color: textPrimary }}>
             <p>
-              Like many, my humble beginnings lie in the worlds of Geocities, Angelfire and Tripod, coding for fun by hand in NotePad (WordPad introduced hidden characters!) and the mid-90s but have since grown and expanded into the modern landscape of robustly-featured internet applications, as the web did.
+              Like many, my humble beginnings lie in the worlds of Geocities, Angelfire and Tripod, coding for fun by hand in NotePad (WordPad introduced hidden characters!) and the mid-90s but have since grown and expanded into the modern landscape of robustly-featured applications as the web did.
             </p>
             <p>
-              My current favorite language is <span className="highlight">JavaScript</span> and I love everything having to do with open source.
+              My current favorite language is <span className="highlight">JavaScript</span> and I love everything having to do with open source. HTML5, CSS3, JavaScript (ES6), React, Flux, Redux, Node, Angular, npm, webpack, yarn, babel and lint are just a few of the other tools in my current toolbox.and I love everything having to do with the open source web.
             </p>
             <img src="http://brendonkuleck.com/images/brendon_profile.png" className="profile-img" style={{ display: "inline" }}/>
             <p>
-              HTML5, CSS3, JavaScript (ES6 // ECMA2015), JS frameworks (<span className="highlight">React</span> (with or without Redux), Node, Angular), npm, yarn, meteor, MongoDB, REST, babel, jslint, webpack, AWS are just a few of the tools in my current toolbox.
+              When it comes to programming paradigms, I'm educated in classical OO but have been digging more and more into the functional programming side of things.
             </p>
-            <p>
-              Professionally, I've developed applications such as the <span className="highlight">NiKEiD Creator</span> as a consultant at <span className="highlight">R/GA</span>, <span className="highlight">Groupon</span>'s web, email and internal tool platforms, the redesigned <span className="highlight">US Dept of Education FAFSA Portal</span> with Accenture and promotional and interactive websites/apps for Fortune 500 companies like HP, Motorola, Burger King, GE, GM, Walgreen's, Johnnie Walker, Land Rover, United and AARP while at <span className="highlight">DesignKitchen</span>.
+            <p>Professionally I've developed applications including the <span className="highlight">NiKEiD Creator</span> with <span className="highlight">R/GA</span>, <span className="highlight">Groupon</span>'s web, email and internal tool platforms, the redesigned <span className="highlight">US Dept of Education FAFSA Portal</span> with Accenture and promotional and interactive websites, games and apps for Fortune 500 companies like HP, Motorola, Burger King, GE, GM, Walgreen's, Johnnie Walker, Land Rover, United and AARP while at <span className="highlight">DesignKitchen</span>.
             </p>
             <p>
               In addition this plethroa of overwhelmingly diverse subjects, I also enjoy reading, listening to music, collecting sneakers, comedy, fishing, hiking, camping, playing hockey and <span className="highlight">basketball</span>, taking pictures of my cat <a href="http://instagram.com/bdkay" className="about-link" rel="noreferrer noopener" target="_blank">Simon</a> and watching orange and brown ball sports. I also love Seinfeld, 90s action movies (especially Van Damme and Seagal) and anything having to do with time travel or personally incomprehensible scientific subjects.
