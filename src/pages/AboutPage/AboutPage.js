f8c476c834315dd900c7c16f49ba7107236fc886
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-30 11:46:36 -0500

    better bio

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -25,18 +25,18 @@ const AboutPage = (props, context) => {
         <div className="about-wrapper">
           <div className="about-content" style={{ color: textPrimary }}>
             <p>
-              Like many, my humble beginnings lie in the worlds of Geocities, Angelfire and Tripod; coding for fun by hand in NotePad (WordPad introduced hidden characters!) and the mid-90s but have since grown and expanded into the modern landscape of robustly-featured applications, as the web did.
+              Like many, my humble beginnings lie in the worlds of Geocities, Angelfire and Tripod; coding for fun by hand in NotePad (WordPad introduced hidden characters!) and the mid-90s but have grown and expanded into the modern landscape of robustly-featured applications as the web did.
             </p>
             <p>
-              My current favorite language is <span className="highlight">JavaScript</span> and I love everything having to do with open source. Lately, I've mostly been writing JavaScript (ES6) with React and Redux and experimenting with PWA concepts.
+              Currently, my favorite language is <span className="highlight">JavaScript</span> and I love everything having to do with the open source community. Lately, I've mostly been writing JavaScript (ES6) with React and Redux and experimenting with PWA concepts.
             </p>
             <img src="http://brendonkuleck.com/images/brendon_profile.png" className="profile-img" style={{ display: "inline" }}/>
-            <p>I've developed applications including the <span className="highlight">NiKEiD Creator</span> with <span className="highlight">R/GA</span>, <span className="highlight">Groupon</span>'s web, email and internal tool platforms, the redesigned <span className="highlight">US Dept of Education FAFSA Portal</span> with Accenture and promotional and interactive websites, games and apps for Fortune 500 companies like HP, Motorola, Burger King, GE, GM, Walgreen's, Johnnie Walker, Land Rover, United and AARP while at <span className="highlight">DesignKitchen</span>.
+            <p>In my professional career I've developed a diverse array of both consumer and business facing applications including the online <span className="highlight">NiKEiD Creator</span>, <span className="highlight">Groupon</span>'s web, email and internal tool platforms, implemented the redesigned <span className="highlight">US Dept of Education FAFSA Portal</span> with Accenture and promotional and interactive websites, games and apps for Fortune 500 companies including HP, Motorola, TD Ameritrade, Burger King, Land Rover, GE, GM, Walgreen's, Johnnie Walker, United and AARP while at <span className="highlight">DesignKitchen</span>.
             </p>
             <p>
               In addition this plethroa of overwhelmingly diverse subjects I also enjoy reading, listening to music, collecting sneakers, comedy, live music, fishing, hiking, camping, playing hockey and basketball (<a href="https://fastmodelsports.com/" className="about-link" rel="noreferrer noopener" target="_blank">professionally</a>), taking pictures of my cat <a href="http://instagram.com/bdkay" className="about-link" rel="noreferrer noopener" target="_blank">Simon</a>🐱 and watching orange and brown ball sports. I also love Seinfeld, 90s action movies (especially Van Damme and Seagal) and anything having to do with time travel or personally incomprehensible scientific subjects.
             </p>
-            <p>The English major side of my brain is also in constant battle with the Engineering side over the Oxford comma's logical usage versus staying DRY.
+            <p>The English major side of my brain and the Engineering side are locked in a persistent, eternal war over the Oxford comma's logical usage versus always staying DRY.
             </p>
             <br />
             <p>
